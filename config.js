da1cf5e645b3bbe6119f8516ba3657f3297386f0
AP Studio <141265995+apdevstudio@users.noreply.github.com>
2023-09-10 19:38:21 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "moviesbackend.tmaa.workers.dev",
+  VITE_CORS_PROXY_URL: "https://movproxbooty.tmaa.workers.dev/",
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZTVhZTM0MTMwMmM2NjZkN2E2NDJhMTYyOTFkYTA0MSIsInN1YiI6IjY0ZmQxZDM5ZGI0ZWQ2MTAzM2EwOTI5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I8oxBwOrTa2WuD0I8-_ZJsbeR_GB5dKaFNAt0lX2XmA"
 };
